3749c96110e9bbe75a0b848b15e8e21be46d2d3e
Sergey Suyagin <59764889+sergeysuyagin@users.noreply.github.com>
2022-11-08 14:38:07 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,14 +1,10 @@
 import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_add_to_cart_button(browser):
+def test_button_add_to_cart(browser): 
     browser.get(link)
-    WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No add to cart button on page")
-    time.sleep(5)
-
+    # time.sleep(30)
+    button = browser.find_element_by_css_selector('button.btn-add-to-basket').get_attribute('value')
+    assert len(button) > 0, 'Add to cart button not found'
