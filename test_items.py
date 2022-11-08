14351ff902d737729695e31a49b2e164ef3cc5f6
Sergey Suyagin <59764889+sergeysuyagin@users.noreply.github.com>
2022-11-08 15:20:00 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,9 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_cart_button_is_on_page(browser):
     browser.get(link)
-    try:
-        browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-        element_is_on_page = True
-    except:
-        element_is_on_page = False
-    assert element_is_on_page, "Add to cart button not found"
+    buttons = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(buttons) > 0, "Add to cart button not found"
